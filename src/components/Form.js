f37121cc9fc3fffec8e8c11ad65e7f516639deda
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-17 14:37:13 +0100

    Refactored prop names for better usability

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,8 @@
 import "../styles/Form.css";
 
-function Form({ handleChange, handleSubmit, title, children }) {
+function Form({ onChange, onSubmit, title, children, defaultObj = {} }) {
   return (
-    <form className="form" name="form" onSubmit={handleSubmit}>
+    <form className="form" name="form" onSubmit={onSubmit}>
       <h3 className="call-to-action">{title}</h3>
 
       <div className="form--grid--container">
@@ -11,7 +11,8 @@ function Form({ handleChange, handleSubmit, title, children }) {
           name="task"
           placeholder="Task"
           type="text"
-          onChange={handleChange}
+          onChange={onChange}
+          value={defaultObj.task}
           required
         />
         <div className="form--title">Project: </div>
@@ -19,15 +20,33 @@ function Form({ handleChange, handleSubmit, title, children }) {
           name="project"
           placeholder="Project"
           type="text"
-          onChange={handleChange}
+          onChange={onChange}
+          value={defaultObj.project}
           required
         />
         <div className="form--title">Date Due: </div>
-        <input name="date" type="date" onChange={handleChange} required />
+        <input
+          name="date"
+          type="date"
+          onChange={onChange}
+          value={defaultObj.date}
+          required
+        />
         <div className="form--title">Time Due: </div>
-        <input name="time" type="time" onChange={handleChange} required />
+        <input
+          name="time"
+          type="time"
+          onChange={onChange}
+          value={defaultObj.time}
+          required
+        />
         <div className="form--title">Priority: </div>
-        <select name="priority" onChange={handleChange} required>
+        <select
+          name="priority"
+          onChange={onChange}
+          value={defaultObj.priority}
+          required
+        >
           <option value="Low">Low</option>
           <option value="Medium">Medium</option>
           <option value="High">High</option>
