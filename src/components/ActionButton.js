3a8308e25cb0f9852d00dbedf612c6a4449dc169
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-17 14:35:11 +0100

    Added another prop to control icon size

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,10 +1,10 @@
-function ActionButton({ action, children, image, alt, className }) {
+function ActionButton({ action, children, image, imageWidth, alt, className }) {
   return (
     <button onClick={action} className={className}>
       <span style={{ padding: 0 }}>{children}</span>
       <img
         src={image}
-        width="15 px"
+        width={`${imageWidth}`}
         alt={alt}
         style={{ padding: 0, margin: 0 }}
         className="icon"
