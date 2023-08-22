8fbb1f6ccc2e814730073f2417a455e98ad07561
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-22 14:52:51 +0100

    Form style classes altered and for prettier rules enforced

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,52 +5,42 @@ function Form({ onChange, onSubmit, title, children, defaultObj = {} }) {
     <form className="form" name="form" onSubmit={onSubmit}>
       <h3 className="call-to-action">{title}</h3>
 
-      <div className="form--grid--container">
-        <div className="form--title">Task: </div>
-        <input
-          name="task"
-          placeholder="Task"
-          type="text"
-          onChange={onChange}
-          value={defaultObj.task}
-          required
-        />
-        <div className="form--title">Project: </div>
-        <input
-          name="project"
-          placeholder="Project"
-          type="text"
-          onChange={onChange}
-          value={defaultObj.project}
-          required
-        />
-        <div className="form--title">Date Due: </div>
-        <input
-          name="date"
-          type="date"
-          onChange={onChange}
-          value={defaultObj.date}
-          required
-        />
-        <div className="form--title">Time Due: </div>
-        <input
-          name="time"
-          type="time"
-          onChange={onChange}
-          value={defaultObj.time}
-          required
-        />
-        <div className="form--title">Priority: </div>
-        <select
-          name="priority"
-          onChange={onChange}
-          value={defaultObj.priority}
-          required
-        >
-          <option value="Low">Low</option>
-          <option value="Medium">Medium</option>
-          <option value="High">High</option>
-        </select>
+      <div className="form-grid-container">
+        <div className="form--input">
+          <div className="form--input-title">Task </div>
+          <input name="task" placeholder="Task" type="text" onChange={onChange} value={defaultObj.task} required />
+        </div>
+
+        <div className="form--input">
+          <div className="form--input-title">Project</div>
+          <input
+            name="project"
+            placeholder="Project"
+            type="text"
+            onChange={onChange}
+            value={defaultObj.project}
+            required
+          />
+        </div>
+
+        <div className="form--input">
+          <div className="form--input-title">Due</div>
+          <input name="date" type="date" onChange={onChange} value={defaultObj.date} required />
+        </div>
+
+        <div className="form--input">
+          <div className="form--input-title">Time</div>
+          <input name="time" type="time" onChange={onChange} value={defaultObj.time} required />
+        </div>
+
+        <div className="form--input">
+          <div className="form--input-title">Priority</div>
+          <select name="priority" onChange={onChange} value={defaultObj.priority} required>
+            <option value="Low">Low</option>
+            <option value="Medium">Medium</option>
+            <option value="High">High</option>
+          </select>
+        </div>
       </div>
       {children}
     </form>
