1a3ca79759f079c1f6888eb001ab5c71befc1a29
usama-bit137 <usamaaqeel98@gmail.com>
2023-08-22 14:49:32 +0100

    Prettier configured rules changed printWidth of code.

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,18 +1,8 @@
 function ActionButton({ action, children, image, imageWidth, alt, className }) {
   return (
-    <button
-      style={{ cursor: "pointer" }}
-      onClick={action}
-      className={className}
-    >
+    <button style={{ cursor: "pointer" }} onClick={action} className={className}>
       <span style={{ padding: 0 }}>{children}</span>
-      <img
-        src={image}
-        width={`${imageWidth}`}
-        alt={alt}
-        style={{ padding: 0, margin: 0 }}
-        className="icon"
-      />
+      <img src={image} width={`${imageWidth}`} alt={alt} style={{ padding: 0, margin: 0 }} className="icon" />
     </button>
   );
 }
